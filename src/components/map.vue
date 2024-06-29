d6ebe875096513617620a165f5863af5f94384b0
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2024-06-29 19:35:45 +0000

    fix

--- src/components/map.vue
+++ src/components/map.vue
@@ -3,8 +3,23 @@ const { map } = defineProps({ map: String })
 </script>
 <template>
   <section class="antialiased" id="map">
-    <div class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-8 lg:py-18">
-      <main class="flex justify-center px-2 lg:px-4 py-10">
+    <div class="max-w-screen-xl px-4 py-6 mx-auto lg:px-6 sm:py-8 lg:py-12">
+      <div class="mt-8 py-8 px-6 mx-auto max-w-screen-xl sm:py-16 lg:px-8">
+        <div class="min-w-max" data-aos="fade-down">
+          <h1
+            class="mb-2 text-5xl sm:text-left tracking-tight font-extrabold text-gray-900 text-center"
+          >
+            地图
+          </h1>
+          <h1
+            class="mb-4 text-2xl sm:text-left tracking-tight font-extrabold text-gray-900 text-center"
+          >
+            Map
+          </h1>
+        </div>
+        <div class="mt-4 sm:text-left text-center">地图会有所更改，敬请留意</div>
+      </div>
+      <main class="flex justify-center px-2 lg:px-4 py-4">
         <div
           class="shadow-[0_45px_70px_-25px_rgba(0,0,0,0.4)] h-[200px] w-[400px] md:w-[600px] md:h-[400px] lg:w-[1200px] lg:h-[700px] rounded-lg"
         >
