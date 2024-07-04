7b1d30e4ae9e1636811e16264a3375fac191ca87
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2024-07-05 00:42:53 +0800

    Update stores.vue

--- src/components/stores.vue
+++ src/components/stores.vue
@@ -6,7 +6,7 @@ const stores = await fetch('https://canteenday-2024.vercel.app/stores').then((r)
 const foodCount = ref(stores.filter((store) => store.category === '主食').length)
 const drinkCount = ref(stores.filter((store) => store.category === '饮料').length)
 const gameCount = ref(stores.filter((store) => store.category === '游戏').length)
-const otherCount = ref(stores.filter((store) => store.category === '其他').length)
+const otherCount = ref(stores.filter((store) => store.category === '甜品').length)
 
 const currentPage = ref('主食')
 const currentShowing = ref(stores.filter((store) => store.category === currentPage.value))
@@ -58,24 +58,24 @@ const changeCurrentPage = (category) => {
           <li>
             <div
               class="flex items-center text-center p-2 rounded-lg"
-              @click="changeCurrentPage('游戏')"
-              :class="currentPage == '游戏' ? 'bg-pink-200 animate-fadeIn' : 'cursor-pointer'"
+              @click="changeCurrentPage('甜品')"
+              :class="currentPage == '甜品' ? 'bg-pink-200 animate-fadeIn' : 'cursor-pointer'"
             >
-              <i class="bx bxs-invader"></i>
+              <i class="bx bx-layer"></i>
               <span class="ms-1 text-xl font-medium text-gray-500 md:ms-2"
-                >游戏 {{ gameCount }}</span
+                >甜品 {{ otherCount }}</span
               >
             </div>
           </li>
           <li>
             <div
               class="flex items-center text-center p-2 rounded-lg"
-              @click="changeCurrentPage('其他')"
-              :class="currentPage == '其他' ? 'bg-pink-200 animate-fadeIn' : 'cursor-pointer'"
+              @click="changeCurrentPage('游戏')"
+              :class="currentPage == '游戏' ? 'bg-pink-200 animate-fadeIn' : 'cursor-pointer'"
             >
-              <i class="bx bx-layer"></i>
+              <i class="bx bxs-invader"></i>
               <span class="ms-1 text-xl font-medium text-gray-500 md:ms-2"
-                >其他 {{ otherCount }}</span
+                >游戏 {{ gameCount }}</span
               >
             </div>
           </li>
