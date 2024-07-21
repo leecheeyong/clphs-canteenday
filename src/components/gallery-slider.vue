2361e4bf51d474aa5309ea83c51329c117e2a81d
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2024-07-21 18:45:01 +0000

    fix

--- src/components/gallery-slider.vue
+++ src/components/gallery-slider.vue
@@ -174,6 +174,18 @@ const changeCurrentPage = (category) => {
       <button
         type="button"
         class="bg-white focus:ring-4 focus:outline-none rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3"
+        @click="changeCurrentPage('乐队')"
+        :class="
+          currentPage == '乐队'
+            ? 'text-blue-700 focus:ring-blue-300 border dark:border-blue-500 dark:text-blue-500 border-blue-600  dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800'
+            : 'cursor-pointer hover:bg-blue-700 hover:text-white dark:text-white focus:ring-gray-300  dark:focus:ring-gray-800 text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700'
+        "
+      >
+        #乐队
+      </button>
+      <button
+        type="button"
+        class="bg-white focus:ring-4 focus:outline-none rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3"
         @click="changeCurrentPage('业者')"
         :class="
           currentPage == '业者'
